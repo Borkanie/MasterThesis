3643fd41ada7ddb75772ff4ebb8df9543bf386eb
Boboc <bogdan.boboc@capgemini.com>
2022-11-17 16:17:11 +0200

    PUI

--- AckermanDraw.py
+++ AckermanDraw.py
@@ -7,8 +7,12 @@ import math
 def drawRectangle(center,size,angle,ax,color="black"): 
     rectangle = patches.Rectangle((center[0],center[1]), size[0], size[1],color=color,angle=angle)
     ax.add_patch(rectangle)
-    
-    
+
+def RotateArray(rotateArrayWIthAngle,angleDegrees):
+    matrix = np.array([rotateArrayWIthAngle[0]*math.cos(angleDegrees)+rotateArrayWIthAngle[1]*math.sin(angleDegrees)
+                        ,-rotateArrayWIthAngle[0]*math.sin(angleDegrees)+rotateArrayWIthAngle[1]*math.cos(angleDegrees)])
+    return matrix
+
 def drawCar(center,length,width,wheelAngle,chasseyAngle = 0):
     # we either have a matrix of four angles or we delete them all
     if len(wheelAngle) != 2 or (len(wheelAngle[0]) != 2 or len(wheelAngle[1]) != 2):
@@ -19,36 +23,25 @@ def drawCar(center,length,width,wheelAngle,chasseyAngle = 0):
     ax = fig.add_subplot(111)
     
     #draw chasis
-    chasisDiag = math.sqrt(width ** 2 + length ** 2)
-    radius = chasisDiag/2
-    chasisCenter=np.array([center[0]+radius*math.cos(chasseyAngle+45),center[1]+radius*math.sin(chasseyAngle+45)])
     drawRectangle(center,np.array([width,length]),chasseyAngle,ax)
 
     #draw wheel
-    chasisWidth = width * math.cos(chasseyAngle)
-    chasislength = length * math.cos(chasseyAngle)
-    chassDegAngle = chasseyAngle *math.pi/180
+    chassDegAngle = -chasseyAngle *math.pi/180
+
     #front left
-    offset = np.array([1.2+(chasisDiag-0.1)*math.cos(chassDegAngle+math.pi*3/4)/2,
-                       (chasisDiag-0.5)*math.sin(chassDegAngle+math.pi*3/4)/2])
-    wheelCenter = chasisCenter + offset
-    drawRectangle(wheelCenter,np.array([width/10,length/10]),chasseyAngle+wheelAngle[0][0],ax,"red")
-    
+    drawRectangle(center+RotateArray(np.array([width*1/8,length*7/8]),chassDegAngle),np.array([width/10,length/10]),chasseyAngle+wheelAngle[0][0],ax,"red")
+
     #front right
-    wheelCenter = np.array([chasisCenter[0]+chasisWidth*3/8,center[1]+chasislength*3/8])
-    drawRectangle(wheelCenter,np.array([width/10,length/10]),chasseyAngle+wheelAngle[0][1],ax,"blue")
-    
+    drawRectangle(center+RotateArray(np.array([width*7/8,length*7/8]),chassDegAngle),np.array([width/10,length/10]),chasseyAngle+wheelAngle[0][1],ax,"blue")
+
     #back left
-    wheelCenter = np.array([center[0]-chasisWidth*3/8,center[1]-chasislength*3/8])
-    drawRectangle(wheelCenter,np.array([width/10,length/10]),chasseyAngle+wheelAngle[1][0],ax,"green")
-    
+    drawRectangle(center+RotateArray(np.array([width*1/8,length*1/8]),chassDegAngle),np.array([width/10,length/10]),chasseyAngle+wheelAngle[1][0],ax,"green")
+
     #back right
-    wheelCenter = np.array([chasisCenter[0]+chasisWidth*3/8,center[1]-chasislength*3/8])
-    drawRectangle(wheelCenter,np.array([width/10,length/10]),chasseyAngle+wheelAngle[1][1],ax,"yellow")
-    
+    drawRectangle(center+RotateArray(np.array([width*7/8,length*1/8]),chassDegAngle),np.array([width/10,length/10]),chasseyAngle+wheelAngle[1][1],ax,"yellow")
+
     plt.grid(True)
-    plt.xlim([-5,5])
-    plt.ylim([-5,5])
+    size = np.array([width,length])
+    plt.xlim([-(size.max()+center.max()),size.max()+center.max()])
+    plt.ylim([-(size.max()+center.max()),size.max()+center.max()])
     plt.show()
-    
-drawCar(np.array([0,0]),4,2,np.array(([15,15],[0,0])),chasseyAngle=45)
